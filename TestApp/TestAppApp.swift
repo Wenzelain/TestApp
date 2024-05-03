e9ff40446c8ea8593d190f5b1dbcf34301d6edca
Wenzelain <mamia_2003@volny.cz>
2024-05-03 23:42:17 +0200

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Vaclav Bohac on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
